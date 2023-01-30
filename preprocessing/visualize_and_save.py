c3c4bfc391e91bd39157af0f91f0aca168b55096
Hobzokatr <hobzova.katerina@gmail.com>
2023-01-30 09:43:36 +0100

    Create visualize_and_save.py

---
+++ preprocessing/visualize_and_save.py
@@ -0,0 +1,17 @@
+
+import sys
+from os import listdir
+from os.path import isfile, join
+import pandas as pd
+from tqdm import tqdm
+from glob import glob
+import csv
+import cv2
+from random import randint
+import random
+import numpy as np
+from load_tools import load_itk, show_images, world_2_voxel
+import SimpleITK as sitk
+from functions import visualize_data
+
+visualize_data()
